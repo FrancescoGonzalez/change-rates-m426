eaf2f2aa283dfef0ac209176a807c0f709d67267
GOCA <goca@moon.corner.tech>
2023-10-19 13:49:24 +0200

    Added files, now the project works

---
+++ src/main/java/org/gonzalez/ConversionServiceResponse.java
@@ -0,0 +1,73 @@
+package org.gonzalez;
+
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class ConversionServiceResponse{
+    private final String from;
+    private final String to;
+    private final Map<String, String> currencyNames = new HashMap<>();
+    private final double[] values = new double[2];
+
+    public ConversionServiceResponse(String body, String from, String to) {
+        this.from = from;
+        this.to = to;
+        try {
+            JSONParser parser = new JSONParser();
+            JSONObject jsonObject = (JSONObject) parser.parse(body); //trasforma un JSON da string a oggetto
+
+            JSONObject rates = (JSONObject) jsonObject.get("rates"); //entriamo nel campo rates, e prendiamo solo le nostre 2 valute volute
+            values[0] = from.equals("EUR")? 1 : (double) rates.get(from);
+            values[1] = to.equals("EUR")? 1 : (double) rates.get(to);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+    public ConversionServiceResponse(String body) {
+        this.from = "";
+        this.to = "";
+        try {
+            JSONParser parser = new JSONParser();
+            JSONObject json = (JSONObject) parser.parse(body);
+
+            JSONObject symbols = (JSONObject) json.get("symbols");
+
+            for (Object key : symbols.keySet()) {
+                String currencyCode = (String) key;
+                String currencyName = (String) symbols.get(key);
+                currencyNames.put(currencyCode, currencyName);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public String getFrom() {
+        return from;
+    }
+
+    public String getTo() {
+        return to;
+    }
+
+    public double getValueFrom() {
+        return values[0];
+    }
+
+    public double getValueTo() {
+        return values[1];
+    }
+
+    public String getFullName(String currency) {
+        for (Map.Entry<String, String> entry : currencyNames.entrySet()) {
+            if (currency.equals(entry.getKey())) {
+                return entry.getValue();
+            }
+        }
+
+        return null;
+    }
+}
---
+++ src/main/java/org/gonzalez/CurrencyService.java
@@ -0,0 +1,52 @@
+package org.gonzalez;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class CurrencyService {
+    private final RemoteCurrentServicePort remoteService;
+
+    public CurrencyService(RemoteCurrentServicePort remoteService) {
+        this.remoteService = remoteService;
+    }
+
+    public double convertAmount(String from, String to, double amount){
+        isValid(from);
+        isValid(to);
+
+        ConversionServiceResponse c = remoteService.callConvertionService(from.toUpperCase(), to.toUpperCase());
+
+        return (amount / c.getValueFrom()) * c.getValueTo();
+    }
+
+    public String getFullName(String currency) {
+        isValid(currency);
+
+        ConversionServiceResponse c = remoteService.callConvertionService(currency.toUpperCase());
+        if (c.getFullName(currency) == null) {
+            throw new InvalidCurrencyException("the currency " + currency + " doesn't exist");
+        }
+
+        return c.getFullName(currency);
+
+    }
+
+    public Map<String, Double> convertMoreAmounts(String from, String... amounts) { // calls convertAmounts(); for every amount, and add the result to an array
+        Map<String, Double> result = new HashMap<>();
+        isValid(from);
+
+        for (String amount : amounts) {
+            isValid(amount);
+            result.put(amount, convertAmount(from, amount, 1));
+        }
+
+        return result;
+
+    }
+
+    public void isValid(String currency) { // if is NOT valid it will throw an exception
+        if (currency.length() != 3) {
+            throw new InvalidCurrencyException("The currency " + currency + " needs to be of 3 letters to be valid");
+        }
+    }
+}
